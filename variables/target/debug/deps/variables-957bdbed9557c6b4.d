8b407acb1f4872fd47b00d155cfe78eef8a586fd
brambt <freez.meinster@gmail.com>
2019-12-30 23:56:56 +0700

    bersih

--- variables/target/debug/deps/variables-957bdbed9557c6b4.d
+++
@@ -1,5 +0,0 @@
-/usr/home/bram/Learn/rust/variables/target/debug/deps/variables-957bdbed9557c6b4: src/main.rs
-
-/usr/home/bram/Learn/rust/variables/target/debug/deps/variables-957bdbed9557c6b4.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/usr/home/bram/Learn/rust/variables/target/debug/variables: /usr/home/bram/Learn/rust/variables/src/main.rs
